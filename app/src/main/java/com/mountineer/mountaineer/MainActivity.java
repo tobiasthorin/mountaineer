464d1fb95411e724c95e1cdf6d1f8be81709df43
Tobias <tobias.ljungstrom@gmail.com>
2016-02-10 12:54:00 +0100

    Add basic GPS functionality

--- app/src/main/java/com/mountineer/mountaineer/MainActivity.java
+++ app/src/main/java/com/mountineer/mountaineer/MainActivity.java
@@ -1,27 +1,74 @@
 package com.mountineer.mountaineer;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+import java.util.jar.Manifest;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    //Private variables
+    private TextView txtLongitude;
+    private TextView txtLatitude;
+    private LocationManager locationManager;
+    private String provider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //Set the view
+        //Set the layout
         setContentView(R.layout.activity_main);
 
-        //The toolbar
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Get the views
+        txtLatitude = (TextView) findViewById(R.id.txtLatitudeValue);
+        txtLongitude = (TextView) findViewById(R.id.txtLongitudeValue);
 
+        //See if we have permission to access location, otherwise ask.
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION, android.Manifest.permission.ACCESS_FINE_LOCATION}, 99);
+        }
+
+        //Get location manager
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        //Set provider criteria (Default) and get location
+        Criteria criteria = new Criteria();
+
+        provider = locationManager.getBestProvider(criteria, false);
+
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        updateCoordinates(location);
+
+    }
+
+    //Updates the text fields in the main interface
+    private void updateCoordinates(Location location) {
+        double lat = location.getLatitude();
+        double lng = location.getLongitude();
+
+        txtLatitude.setText(String.valueOf(lat));
+        txtLongitude.setText(String.valueOf(lng));
     }
 
     @Override
@@ -44,5 +91,26 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Toast.makeText(this, "Enabled provider: " + provider, Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(this, "Disabled provider: " + provider, Toast.LENGTH_SHORT).show();
     }
 }
